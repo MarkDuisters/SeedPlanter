26571cc83cdf6f5d5a39e858423737860327f766
Mark <freak_mania@hotmail.com>
2025-04-11 02:31:55 +0200

    refactored and reworked spawning system. System now first generates positions and keeps track of occupation state. Added support for raymarching, gravity and wind are also applied.

---
+++ Assets/SeedPlanter/Scripts/OccupiedPositionInfo.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using System;
+using UnityEngine;
+
+[Serializable]
+public class OccupiedPositionInfo
+{
+    [ReadOnly] public Vector3 position;
+    [ReadOnly] public bool occupied;
+    [ReadOnly] public float angleNormal;
+    [ReadOnly] public Vector3 normal;
+
+    public OccupiedPositionInfo(Vector3 position, bool occupied, float angleNormal, Vector3 normal)
+    {
+        this.position = position;
+        this.occupied = occupied;
+        this.angleNormal = angleNormal;
+        this.normal = normal;
+    }
+}
+
+#endif
